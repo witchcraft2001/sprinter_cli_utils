2d190590c95db949f3b4ad18dd61c13120f534e0
DmitryM <mikhaltchenkov@gmail.com>
2026-05-06 23:06:25 +0500

    fix(make): read target mtimes from opened files

--- make/src/fs_dss.c
+++ make/src/fs_dss.c
@@ -1,25 +1,67 @@
 #include "make.h"
 
+static u8 g_make_dt_fd;
+static u16 g_make_dt_year;
+static u8 g_make_dt_month;
+static u8 g_make_dt_day;
+static u8 g_make_dt_hour;
+static u8 g_make_dt_minute;
+static u8 g_make_dt_second;
+
+static u8 make_getdt_call(void) __naked {
+    __asm
+        push    ix
+        ld      a, (_g_make_dt_fd)
+        ld      c, #0x17
+        rst     #0x10
+        jr      c, _make_getdt_err
+        ld      a, d
+        ld      (_g_make_dt_day), a
+        ld      a, e
+        ld      (_g_make_dt_month), a
+        ld      a, h
+        ld      (_g_make_dt_hour), a
+        ld      a, l
+        ld      (_g_make_dt_minute), a
+        ld      a, b
+        ld      (_g_make_dt_second), a
+        ld      (_g_make_dt_year), ix
+        pop     ix
+        ld      hl, #0x0000
+        ret
+_make_getdt_err:
+        pop     ix
+        ld      hl, #0x0001
+        ret
+    __endasm;
+}
+
 int fs_get_mtime(const char *path, unsigned int *date, unsigned int *time) {
-    ffblk found;
     i16 fd;
-    int rc;
+    unsigned int year;
 
     fd = dss_open(path, O_RDONLY);
     if (fd < 0) {
         MAKE_LOG("make: mtime miss '%s'\n", path);
         return 0;
     }
-    dss_close((u8)fd);
 
-    rc = findfirst(path, &found, 0x3F);
-    if (rc != 0) {
+    g_make_dt_fd = (u8)fd;
+    if (make_getdt_call() != 0u) {
+        dss_close((u8)fd);
         MAKE_LOG("make: mtime miss '%s'\n", path);
         return 0;
     }
+    dss_close((u8)fd);
 
-    *date = found.date;
-    *time = found.time;
+    year = g_make_dt_year;
+    if (year >= 1980u) {
+        year -= 1980u;
+    }
+    *date = (unsigned int)((year << 9) | ((unsigned int)g_make_dt_month << 5) | (unsigned int)g_make_dt_day);
+    *time = (unsigned int)(((unsigned int)g_make_dt_hour << 11) |
+                           ((unsigned int)g_make_dt_minute << 5) |
+                           ((unsigned int)g_make_dt_second >> 1));
     MAKE_LOG("make: mtime '%s' date=%u time=%u\n", path, *date, *time);
     return 1;
 }
